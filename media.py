f265a0157b4614d4063d3602c2b751b83153e4c7
Amit Shokeen <amitshokeen2014js@gmail.com>
2017-10-03 10:34:41 +0530

    meeting pep8 standards

--- media.py
+++ media.py
@@ -1,10 +1,12 @@
 import webbrowser
 
+
 class Movie():
-    '''This class is used to store movie related information'''
-    #class variable
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #class variable
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    # This class is used to store movie related information
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]  # class variable
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
